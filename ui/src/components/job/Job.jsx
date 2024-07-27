90462891fa1a4336d92791da205bdcc494304886
desousamagustin <agus.desousa99@gmail.com>
2024-07-27 19:44:27 -0300

    Add a component for creation of new jobs in experience section instead of create too much html tags

--- ui/src/components/job/Job.jsx
+++ ui/src/components/job/Job.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import './job.css'
 
-function Job() {
+function Job(props) {
   return (
-    <div>Job</div>
+    <article className="experience__job">
+      <h3 className="experience_subcontainer__title">{props.jobTitle}</h3>
+      <div className="job__data">
+        <h4 className="job__subtitle">{props.company}</h4>
+        <h4 className="job__subtitle">{props.date}</h4>
+      </div>
+      <p className="job__description">
+        {props.description}
+        <ul className="job__list">
+          <li className="job__item">
+            <h5 className="job__item_title">{props.skillTitle1}</h5>
+          </li>
+          <li className="job__item">
+            <h5 className="job__item_title">{props.skillTitle2}</h5>
+          </li>
+          <li className="job__item">
+            <h5 className="job__item_title">{props.skillTitle3}</h5>
+          </li>
+          <li className="job__item">
+            <h5 className="job__item_title">{props.skillTitle4}</h5>
+          </li>
+          <li className="job__item">
+            <h5 className="job__item_title">{props.skillTitle5}</h5>
+          </li>
+        </ul>
+      </p>
+    </article>
   )
 }
 
-export default Job
+export default Job;
--- ui/src/pages/experience/Experience.jsx
+++ ui/src/pages/experience/Experience.jsx
@@ -1,70 +1,36 @@
 import React from 'react'
 import './experience.css'
 
+import Job from '../../components/job/Job'
+
 function Experience() {
   return (
     <div id='experience' className='container'>
       <section className="experience__container">
         <h2 className="experience__title">Professional experience</h2>
-          <article className="experience__job">
-            <h3 className="experience_subcontainer__title">Testing internship</h3>
-            <div className="job__data">
-              <h4 className="job__subtitle">Planexware S.A.</h4>
-              <h4 className="job__subtitle">July 2023 - Present</h4>
-            </div>
-            <p className="job__description">
-              As a QA Tester, I focus on garantee the software quality through manual test executions and collaboration with development team.
-              I create test plans, design and execute test cases. I participate in meetings where new features and improvements are exposed.
-              <ul className="job__list">
-                <li className="job__item">
-                  <h5 className="job__item_title">Microsoft Azure for test plans</h5>
-                </li>
-                <li className="job__item">
-                  <h5 className="job__item_title">SQL</h5>
-                </li>
-                <li className="job__item">
-                  <h5 className="job__item_title">Designing and creation of test cases</h5>
-                </li>
-                <li className="job__item">
-                  <h5 className="job__item_title">Unitary, integration and regression tests</h5>
-                </li>
-                <li className="job__item">
-                  <h5 className="job__item_title">Report of bugs</h5>
-                </li>
-              </ul>
-            </p>
-          </article>
+          <Job
+            jobTitle = 'Testing internship'
+            company = 'Planexware S.A.'
+            date = 'July 2023 - Present'
+            description = 'As a QA Tester, I focus on garantee the software quality through manual test executions and collaboration with development team. I create test plans, design and execute test cases. I participate in meetings where new features and improvements are exposed.'
+            skillTitle1 = 'Microsoft Azure for test plans'
+            skillTitle2 = 'SQL'
+            skillTitle3 = 'Designing and creation of test cases'
+            skillTitle4 = 'Unitary, integration and regression tests'
+            skillTitle5 = 'Reporting of bugs'
+          ></Job>
 
-          <article className="experience__job">
-            <h3 className="experience_subcontainer__title">GIBIO</h3>
-            <div className="job__data">
-              <h4 className="job__subtitle">UTN FRBA</h4>
-              <h4 className="job__subtitle">March 2022 - March 2023</h4>
-            </div>
-            <p className="job__description">
-              I had to design and build a platform from scratch where a student can create his own account and ask for a medical shift.
-              This development includes: a landing page where users can create an account or see important information about GIBIO, 
-              several user interfaces where users can login with his email and password, backend for login, sign up and complete a form and
-              a database for persist the user information using the Hibernate ORM. Many highlights are, for example, using of MVC pattern and object-oriented programming.
-              <ul className="job__list">
-                <li className="job__item">
-                  <h5 className="job__item_title">React</h5>
-                </li>
-                <li className="job__item">
-                  <h5 className="job__item_title">Spring with Java</h5>
-                </li>
-                <li className="job__item">
-                  <h5 className="job__item_title">Hibernate</h5>
-                </li>
-                <li className="job__item">
-                  <h5 className="job__item_title">MVC pattern</h5>
-                </li>
-                <li className="job__item">
-                  <h5 className="job__item_title">Object-oriented programming</h5>
-                </li>
-              </ul>
-            </p>
-          </article>
+          <Job
+            jobTitle = 'GIBIO'
+            company = 'UTN FRBA'
+            date = 'March 2022 - March 2023'
+            description = 'I had to design and build a platform from scratch where a student can create his own account and ask for a medical shift. This development includes: a landing page where users can create an account or see important information about GIBIO, several user interfaces where users can login with his email and password, backend for login, sign up and complete a form and a database for persist the user information using the Hibernate ORM. Many highlights are, for example, using of MVC pattern and object-oriented programming.'
+            skillTitle1 = 'React'
+            skillTitle2 = 'Spring with Java'
+            skillTitle3 = 'Hibernate'
+            skillTitle4 = 'MVC pattern'
+            skillTitle5 = 'Object-oriented programming'
+          ></Job>
       </section>
     </div>
   )
